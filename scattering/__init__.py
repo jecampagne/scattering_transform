45acde5db2fb28759192def6eda80ea04f83ad3f
Jean-Eric Campagne <campagne@lal.in2p3.fr>
2023-05-12 13:45:32 +0200

    fix

--- scattering/__init__.py
+++ scattering/__init__.py
@@ -116,13 +116,14 @@ Use * or + to connect more than one condition.
             func_s = lambda x: st_calc.scattering_coef(x)['for_synthesis']
 
         if estimator_name=='s_mean_extend':
-            def func_s(image):
+            def func(image):
                 N_image = image.shape[0]
                 mean_std = (image.mean((-2,-1))/image.std((-2,-1)))[:,None]
                 s_mean_set = st_calc.scattering_coef(image)
                 p00 = s_mean_set['P00'].reshape((N_image, -1)).log()
                 coeffs = s_mean_set['for_synthesis']
                 return torch.cat((mean_std,p00,coeffs),dim=-1)
+            func_s = lambda x: func(x)
 
 
         if 's_mean_func' in estimator_name: #JEC
